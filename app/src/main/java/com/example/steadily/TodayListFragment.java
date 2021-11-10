a37cf24a2fc5ef8e595b1f052312f6825bbaaf45
uyeah0 <s2031@e-mirim.hs.kr>
2021-11-11 01:47:20 +0900

    오늘 실천 주달력 layout 추가

--- app/src/main/java/com/example/steadily/TodayListFragment.java
+++ app/src/main/java/com/example/steadily/TodayListFragment.java
@@ -1,13 +1,19 @@
 package com.example.steadily;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,17 +21,52 @@ import androidx.fragment.app.Fragment;
 
 // 오늘 실천
 public class TodayListFragment extends Fragment {
+    Activity mActivity;
+
+    ImageButton addScheduleButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
+        mActivity = getActivity();
 
-        View v = inflater.inflate(R.layout.fragment_todaylist, container, false);
+        View view = inflater.inflate(R.layout.fragment_todaylist, container, false);
 
-        CalendarView calendarView = v.findViewById(R.id.calendar);
+        TextView ymTextView = view.findViewById(R.id.ymTextView); //0000년 00월 텍스트뷰
+        TextView[] wDate = new TextView[7];
+        wDate[0] = view.findViewById(R.id.SUN_num); //일 ~ 토 날짜표시 텍스트뷰
+        wDate[1] = view.findViewById(R.id.MON_num);
+        wDate[2] = view.findViewById(R.id.TUE_num);
+        wDate[3] = view.findViewById(R.id.WED_num);
+        wDate[4] = view.findViewById(R.id.THU_num);
+        wDate[5] = view.findViewById(R.id.FRI_num);
+        wDate[6] = view.findViewById(R.id.SAT_num);
 
-        return v;
+        addScheduleButton = view.findViewById(R.id.imgbtnAddSchedule);
+        addScheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddScheduleDialog addScheduleDialog = new AddScheduleDialog(mActivity);
+                addScheduleDialog.showDialog();
+            }
+        });
+
+        return view;
     }
+
+/*    *//*오늘 실천 추가 디자인 함수*//*
+    public class ShowAddDialog{
+        public void showDialog(Activity activity){
+            final Dialog dialog = new Dialog(activity);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); //  nmm,
+            dialog.setContentView(R.layout.activity_dialog_add);
+
+            *//*모양만들기*//*
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); // 투명
+
+            dialog.show(); // 다이얼로그 띄우기
+        }
+    }*/
 
 }
