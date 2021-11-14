89e6c7ad029aa87bdb022b4c48a52ecbcbb84595
chaewon0228 <s2033@e-mirim.hs.kr>
2021-11-14 16:11:16 +0900

    디자인만

--- app/src/main/java/com/example/steadily/CompletedListFragment.java
+++ app/src/main/java/com/example/steadily/CompletedListFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,17 +12,15 @@ import androidx.fragment.app.Fragment;
 
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
+import java.util.Calendar;
+
 // 전체 목록
 public class CompletedListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-
         View view = inflater.inflate(R.layout.fragement_completedlist, container, false);
-
-        MaterialCalendarView materialCalendarView = view.findViewById(R.id.calendarView);
-
 
         return view;
     }
--- app/src/main/java/com/example/steadily/RegisterActivity.java
+++ app/src/main/java/com/example/steadily/RegisterActivity.java
@@ -107,6 +107,7 @@ public class RegisterActivity extends AppCompatActivity {
                                     hashMap.put("title", "e");
                                     hashMap.put("time", "e");
                                     hashMap.put("done", "e");
+                                    hashMap.put("cnt", "0");
                                     hashMap.put("good", "0");
                                     hashMap.put("post_q", "e");
 
