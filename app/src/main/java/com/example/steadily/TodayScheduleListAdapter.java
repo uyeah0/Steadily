0b8669294a418d86129569cea047fd18c36bb044
uyeah0 <s2031@e-mirim.hs.kr>
2021-11-19 13:33:04 +0900

    주캘린더 디자인, 타이머 연결 구성

--- app/src/main/java/com/example/steadily/TodayScheduleListAdapter.java
+++ app/src/main/java/com/example/steadily/TodayScheduleListAdapter.java
@@ -9,6 +9,8 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -50,10 +52,13 @@ public class TodayScheduleListAdapter extends BaseAdapter {
             convertView = inflater.inflate(R.layout.listitem, parent, false);
         }
 
+
+        RelativeLayout todaySchedule_RealativeLayout = convertView.findViewById(R.id.todaySchedule_RealativeLayout);
         CheckBox isDoneSchedule = convertView.findViewById(R.id.todaySchedule_chk);
         TextView title = convertView.findViewById(R.id.todaySchedule_title);
         TextView time = convertView.findViewById(R.id.todaySchedule_time);
         ImageButton modify = convertView.findViewById(R.id.todaySchedule_modify_btn);
+        LinearLayout todaySchedule_LinearLayout = convertView.findViewById(R.id.todaySchedule_LinearLayout);
         TextView repeatCount = convertView.findViewById(R.id.todaySchedule_num);
 
 
@@ -64,6 +69,35 @@ public class TodayScheduleListAdapter extends BaseAdapter {
         time.setText(item.time);
         repeatCount.setText(Integer.toString(item.repeatCount));
 
+        todaySchedule_RealativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, TimerActivity.class);
+                intent.putExtra("title", item.title);
+                context.startActivity(intent);
+            }
+        });
+
+        /*timer 화면 실행*/
+        isDoneSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, TimerActivity.class);
+                intent.putExtra("title", item.title);
+                context.startActivity(intent);
+            }
+        });
+
+        todaySchedule_LinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, TimerActivity.class);
+                intent.putExtra("title", item.title);
+                context.startActivity(intent);
+            }
+        });
+
+        /*타이틀 눌렀을 시 timer 화면 실행*/
         title.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -73,6 +107,8 @@ public class TodayScheduleListAdapter extends BaseAdapter {
             }
         });
 
+        /*수정 버튼 눌렀을 시 수정 화면 실행*/
+        /*현재 토스트로 구현*/
         modify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -80,6 +116,12 @@ public class TodayScheduleListAdapter extends BaseAdapter {
             }
         });
 
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "이미지 버튼이 눌려졌습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
         return convertView;
